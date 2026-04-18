c51a2af6b8715c56889a03d38228223c71d1fdee
Trevor Sullivan <trevors1775@gmail.com>
2026-04-18 19:13:08 -0400

    models changes

--- backend/models.py
+++ backend/models.py
@@ -10,7 +10,6 @@ class Account(Base):
     profile_id    = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     email         = Column(String, unique=True, nullable=False)
     password_hash = Column(String, nullable=False)
-    verified      = Column(Boolean, nullable=False, default=False)
     created_at    = Column(DateTime(timezone=True), server_default=func.now())
     last_login    = Column(DateTime(timezone=True), nullable=True)
 
